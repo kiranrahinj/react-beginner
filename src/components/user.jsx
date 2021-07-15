f5f2bededbe8779b8bcc786fd62d188317912b8d
kiranrahinj <rahinjk1@gmail.com>
2021-07-15 14:34:45 +0530

    life cycle code

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,32 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const User = (props) => {
-  const [planet, setPlanet] = useState("earth");
+class User extends React.Component {
+  constructor(props) {
+    super(props);
 
-  //component did mount
-  useEffect(() => {
- //   setPlanet("mars");
+    this.state = {
+      planet: "jupiter",
+    };
+    console.log("hey im from constructor");
+  }
+  
+componentDidMount (){
+    this.setState({
+        planet:"pluto"
+    })
+    console.log("im from component did mount");
+}
 
-    console.log("component mounting");
-    return console.log("bye bye");
+  shouldComponentUpdate(nextProp, nextState) {
+    console.log("from component update");
+    console.log({
+      nextProp,
+      nextState,
+    });
+    return true;
     
-  },[]);
-  //component did update
-  //should component update
-  useEffect(()=>{
-    console.log("planet changes");
-   
+  }
 
-  },[planet]);
+  getSnapshotBeforeUpdate(prevProp, prevState) {
+    console.log("from snapshot");
+    console.log({ prevProp, prevState });
+    return true;
+  }
 
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>{props.desc}</p>
-      <button onClick={()=>setPlanet("pluto")}>{planet}</button >
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
-    </div>
-  );
-};
-
+  render() {
+    console.log("im from render");
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.desc}</p>
+        <h4>{this.state.planet}</h4>
+      </div>
+    );
+  }
+}
 export default User;
